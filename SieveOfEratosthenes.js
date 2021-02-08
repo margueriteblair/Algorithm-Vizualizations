87d0a70fd542a21691adb35a96bb56f348a84212
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-08 13:21:30 -0500

    turning it into an object

--- SieveOfEratosthenes.js
+++ SieveOfEratosthenes.js
@@ -1,5 +1,27 @@
-function createCrid(num) {
-    for (let i = 1; i <= num; i++) {
-        console.log(i);
+document.getElementById("welcome").innerText = "Welcome to Margie's Viz of the Sieve of Eratosthenes";
+document.getElementById("instructions").innerText = "Input the number you want to see go through the sieve:";
+
+num = document.getElementById("sieve_num");
+
+let sieve = {
+    createGrid: function(num) {
+        let container = document.createElement('div');
+        container.id = "main";
+        container.className = "container";
+        for (let i = 1; i <= num; i++) {
+            let row = document.createElement('div');
+            row.className = "row"
+            row.id = "row" + i;
+            for (let j = 1; j <= num; j++) {
+                let box = document.createElement('div');
+                box.className='box';
+                box.innerText = (i*j).toString();
+                row.appendChild(box);
+            }
+            container.appendChild(row);
+        }
+        return container;
     }
 }
+
+// console.log(createGrid(5));
