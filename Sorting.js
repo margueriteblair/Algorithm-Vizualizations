856bb3a19da5ff6fb014a39a8fb0a0e1ff9ee890
Marguerite Blair <marguerite.blair@gmail.com>
2021-02-13 19:21:34 -0500

    sorting algo set up and not much else for today

---
+++ Sorting.js
@@ -0,0 +1,24 @@
+document.getElementById("welcome_sort").innerText = "Welcome to Margie's sorting Viz!"
+
+
+let sort = {
+    mergeSort: function() {
+        //merge sort code
+    },
+
+    quickSort: function(){
+        //quick sort codel
+    },
+
+    heapSort() {
+        //heap sort code
+    },
+
+    bubbleSort() {
+        //bubble sort code
+    },
+
+    resetArray() {
+        //reset array here
+    }
+}
